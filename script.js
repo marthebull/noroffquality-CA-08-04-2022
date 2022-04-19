468983f7b4b0d5603bc44b227d1f746de5c75545
Sanhamm <sanhamm097@gmail.com>
2022-04-19 12:40:14 +0200

    heiehi

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ fetch("http://marthebull.no/cms/wp-json/tribe/events/v1/events", {
 	"method": "GET"
 })
 .then(response => {
-	console.log(response);
+	//console.log(response);
     return response.json();
 })
 //sortert alt etter nærmeste dato.
@@ -13,36 +13,48 @@ fetch("http://marthebull.no/cms/wp-json/tribe/events/v1/events", {
             return new Date(a.start_date) - new Date(b.start_date)
         })
         listEvents(data.events)
+        
 
     }
 })
-
 let list = document.querySelector("div#hovedKalender");
+let miniList = document.querySelector("div#miniKalender");
 
-const listEvents = (events) => {
+const listEvents = (sortedList) => {
     list.innerHTML = "";
 
-
-    events.forEach(ev => {
-        console.log(ev);
-    })
-
-    // for(let ev of events) {
-    //     console.log(ev);
-    // }
+    //console.log(sortedList);
     
     //laget en ny array for de objektene vi skal bruke i oppgaven
-    let newArray = events.map(ev => ({title: ev.title, date: ev.start_date, description: ev.excerpt, tags: ev.tags }))
-    console.log(newArray);
+    let mappedList = sortedList.map(ev => ({title: ev.title, date: ev.start_date, description: ev.excerpt, tags: ev.tags }))
+    //console.log(mappedList);
 
-    newArray.forEach(ev2 => {
+
+    //dette er hoved listen
+    
+    mappedList.forEach(ev2 => {
         let newList = `
             <div class="event-card">
+                <p>${ev2.date}</p>
                 <p class="event-header"><strong>${ev2.title}</strong></p>
                 <p>${ev2.description}</p>
             </div>`;
         list.innerHTML += newList;
     }) 
+
+    // dette er Den lille kalender øverst på siden
+    const miniKalender = mappedList.slice(0, 3)
+    //console.log(miniKalender);
+
+    miniKalender.forEach(ev3 => {
+        let miniList1 = `
+            <div class="event-card">
+                <p>${ev3.date}</p>
+                <p class="event-header"><strong>${ev3.title}</strong></p>
+                <p>${ev3.description}</p>
+            </div>`;
+        miniList.innerHTML += miniList1;
+
+
+    })
 }
-
-

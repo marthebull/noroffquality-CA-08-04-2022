e464851f2d28be7c79b1d6f64c21998c54854bfe
marthebull <marthebullpettersen@gmail.com>
2022-04-22 11:28:53 +0200

    alt funker

--- script.js
+++ script.js
@@ -27,36 +27,40 @@ fetch("http://marthebull.no/cms/wp-json/tribe/events/v1/events?per_page=99", {
 
 let list = document.querySelector("div#hovedKalender");
 let miniList = document.querySelector("div#miniKalender");
-
+const months = ["JAN", "FEB", "MAR", "APR", "MAI", "JUN", "JUL", "AUG", "SEP", "OKT", "NOV", "DES"];
 
 let listEvents = (sortedList) => {
     list.innerHTML = "";
-    //console.log(sortedList);
-
+    console.log(sortedList);
     //laget en ny array for de objektene vi skal bruke i oppgaven
     let mappedList = sortedList.map(ev => ({title: ev.title, date: ev.start_date, description: ev.excerpt, tags: ev.tags }))
     //console.log(mappedList);
 
-
     //dette er hovedlisten
     const hovedKalender = mappedList.slice(0, 10)
     //console.log(hovedKalender);
-
     hovedKalender.forEach(ev => {
+        let eventDate = new Date(ev.date);
+        //console.log(eventDate);
+        let eventMonth = eventDate.getMonth();
+        //console.log(months[eventMonth]);
         let newList =`
             <div class="event-card">
-                <p>${ev.date}</p>
+                <div class="date-cont">
+                    <p class="day">${eventDate.getDate()}</p>
+                    <p class="month">${months[eventMonth]}</p>
+                </div>
                 <details>
-                    <summary class="event-header"><strong>${ev.title}</strong></summary>
+                    <summary class="event-header">
+                    <strong>${ev.title}<i class="fas fa-chevron-down"></i></strong>
+                    </summary>
                     <p>${ev.description}</p>
                 </details>
             </div>`;
         list.innerHTML += newList;
     }) 
-
-
-
 }
+
 
 
 // lister ut minikalenderen på siden, sortert og mappet
@@ -64,16 +68,23 @@ let listEvents = (sortedList) => {
 let listEvents1 = (sortedList1) => {
     miniList.innerHTML = "";
     //console.log(sortedList1);
-
     //laget en ny array for de objektene vi skal bruke i oppgaven
     let mappedList1 = sortedList1.map(ev => ({title: ev.title, date: ev.start_date, description: ev.excerpt, tags: ev.tags }))
     //console.log(mappedList);
-     const miniKalender = mappedList1.slice(0, 3)
+    const miniKalender = mappedList1.slice(0, 3)
     //console.log(miniKalender);
     miniKalender.forEach(ev => {
+        //console.log(ev.date);
+        let eventDate = new Date(ev.date);
+        //console.log(eventDate);
+        let eventMonth = eventDate.getMonth();
+        //console.log(months[eventMonth]);
         let miniList1 = `
             <div class="event-card" >
-                <p>${ev.date}</p>
+                <div class="date-cont">
+                    <p class="day">${eventDate.getDate()}</p>
+                    <p class="month">${months[eventMonth]}</p>
+                </div>
                 <p class="event-header"><strong>${ev.title}</strong></p>
                 
             </div>`;
@@ -109,6 +120,7 @@ let filterFunction = () => {
         filteredList = myList.filter((item)=>{
             //console.log("This is the item: "+item.tags);
             console.log(item.tags);
+            console.log(item.start_date_details);
             //console.log("This is the filter: "+filterRoller);
             for (let tag of item.tags) {
                 //console.log(tag.id);
@@ -126,6 +138,30 @@ let filterFunction = () => {
         // let filterPeriode= [];
         // filterPeriode.push(periodeSelector.value)
         // console.log(filterPeriode);
+
+
+        let filterSemester = [];
+        filterSemester.push(semesterSelector.value);
+        console.log(filterSemester);
+    
+        if (filterSemester != "all") {
+            console.log(filteredList);
+            filteredList = filteredList.filter((item) => {
+                //console.log("This is the item: "+item.tags);
+                //console.log(item.tags);
+                //console.log("This is the filter: "+filterKategori);
+                for (let tag of item.tags) {
+                    if (tag.id == filterSemester) {
+                        return true;
+                    }
+                }
+                return false;
+            });
+        }
+        console.log(filteredList);
+
+
+
 
     //kategori selector
     let filterKategori = [];
